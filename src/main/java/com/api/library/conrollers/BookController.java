8d29a7b4820362e81fb741ca80c6a0ac2bb7eccb
Nejemeddine Tabbebi <ntabbebi@gmail.com>
2024-05-08 17:00:10 +0200

    Get /books endpoint

--- src/main/java/com/api/library/conrollers/BookController.java
+++ src/main/java/com/api/library/conrollers/BookController.java
@@ -4,18 +4,22 @@ package com.api.library.conrollers;
 import com.api.library.model.Book;
 import com.api.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
+@RequestMapping("/api/v1")
 public class BookController {
 
     @Autowired
     private BookService bookservice;
     @GetMapping("/books")
     List<Book> findAllBooks() {
-        return null;
+       return bookservice.getBooks();
     }
 }
