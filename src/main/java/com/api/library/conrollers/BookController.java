151748a0cad202c237f214b49d94ea8eec297401
Nejemeddine Tabbebi <ntabbebi@gmail.com>
2024-05-10 15:11:06 +0200

    Refactor Delete method

--- src/main/java/com/api/library/conrollers/BookController.java
+++ src/main/java/com/api/library/conrollers/BookController.java
@@ -3,6 +3,7 @@ package com.api.library.conrollers;
 
 import com.api.library.entities.Book;
 import com.api.library.services.BookService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,30 +17,42 @@ import java.util.List;
 public class BookController {
 
     @Autowired
-    private BookService bookservice;
+    private BookService bookService;
+
     @GetMapping("/books")
     List<Book> findAllBooks() {
-       return bookservice.getBooks();
+       return bookService.getBooks();
     }
     @GetMapping("/book")
     public ResponseEntity<Book> getBook(@RequestParam long bookId) {
-        Book book = bookservice.getBook(bookId);
+        Book book = bookService.getBook(bookId);
         return new ResponseEntity<>(book, HttpStatus.OK);
     }
     @GetMapping("/booksPrices")
     public List<Book> getProduct(@RequestParam double priceStart,@RequestParam double priceEnd ) {
-        List<Book> books = bookservice.getBooksByPrices(priceStart,priceEnd);
+        List<Book> books = bookService.getBooksByPrices(priceStart,priceEnd);
         return books;
     }
 
     @PatchMapping("/book")
     public ResponseEntity<Book> updateBorrowingStatus(@RequestParam long bookId){
-          Book updatedBook= bookservice.updateBorrowingStatus(bookId);
+          Book updatedBook= bookService.updateBorrowingStatus(bookId);
           return new ResponseEntity<>(updatedBook,HttpStatus.OK);
     }
 
     @DeleteMapping("/book")
     public ResponseEntity<Book> deleteBook(@RequestParam long bookId) {
-        bookservice.deleteBook(bookId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);    }
+        try {
+            bookService.deleteBook(bookId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (EntityNotFoundException e) {
+            // Return a 404 Not Found if the book doesn't exist
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            // Catch other exceptions and return a 500 Internal Server Error
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
 }
