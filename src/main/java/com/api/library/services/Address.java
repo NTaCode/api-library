2a14e949662349ab48d5238651e0d5b172d6240e
Nejemeddine Tabbebi <ntabbebi@gmail.com>
2024-05-12 20:49:02 +0200

    adding address class

--- src/main/java/com/api/library/services/Address.java
+++ src/main/java/com/api/library/services/Address.java
@@ -1,2 +1,10 @@
-package com.api.library.services;public class Address {
+package com.api.library.services;
+
+public class Address {
+
+    private int streetNumber;
+    private String streetName;
+    private int zipCode;
+    private int etage;
+    private int appNumber;
 }
